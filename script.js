fc4964f5ccde3cbee2e0253f33cd5469ecf27ece
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-07-01 08:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function fibonacci(num) {
-// your code here
+  if (num === 0 ||num === 1 ) {
+    return 0;
+  }
+   if (num === 2 ) {
+    return 1;
+  }
+
+  var prev = 0;
+  var current = 1;
+
+  for (var i = 3; i <= num; i++) {
+    var next = prev + current;
+    prev = current;
+    current = next;
+  }
+
+  return current;
 }
+/* var num = 5;
+var result = fibonacci(num);
+console.log(result);  */
+
+
 
 module.exports = fibonacci;
